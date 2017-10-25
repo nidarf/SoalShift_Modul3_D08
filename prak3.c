7bceb9faf317797e54158eaf5a2b2ed5b6895b50
Rizvi Sofbrina <rizvisfbrina@gmail.com>
2017-10-25 16:59:34 +0700

    test

--- prak3.c
+++ prak3.c
@@ -10,19 +10,19 @@ int status[2];
 void* playandcount(void *arg)
 {
     pthread_t id=pthread_self();
-   if(pthread_equal (id,tid[1])==0){
+   if(tid[1]==0){
 	int ikan;
 	while(status[1]>=1 && status[2]>=1 && status[1]<=100 && status[2]<=100){
-	printf("Beri Makan Ikan\n1. Lohan %d\n2. Kepiting %d\n", status[1], status[2]);
+	printf("Beri Makan Ikan : \n1. Lohan %d\n2. Kepiting %d\n", status[1], status[2]);
 	scanf("%d", &ikan);
 	if(ikan == 1) status[1]+=10;
 	else status[2]+=10;
 	sleep(1);
-        status[0]-=15;
+        status[1]-=15;
 }
 printf("Gagal\n");
 } 
-else if(pthread_equal(id,tid[2])==0){
+else if(tid[2]==0){
   while(status[1]>=1 && status[2] >= 1 && status[1]<=100 && status[2]<=100){ 
         sleep(1);
         status[1]-=10;
@@ -38,7 +38,7 @@ int main(void)
    status[2]=100;
    int i=0;
    int err;
-   while(i<3){
+   while(i<=2){
    	err=pthread_create(&(tid[i]),NULL,&playandcount, NULL);
 	i++;
    }
