64282f8f6f8e48172f51b1fbe12f3acd3aeb2964
nidarf <32903561+nidarf@users.noreply.github.com>
2017-10-25 16:47:50 +0700

    Add files via upload

---
+++ coba4.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+pthread_t tid[3];
+int arr[3];
+int length=157;
+
+void* factorial(void *arg)
+{
+    //unsigned long i=0;
+    int n, i,t, val, j;
+    unsigned long long factorial = 1;
+    
+    pthread_t id=pthread_self();
+    
+    if(pthread_equal(id,tid[0])==0)//thread untuk menjalankan counter
+    {
+	n=arr[0];
+       for(i=1; i<=n; ++i)
+        {
+            factorial *= i;              // factorial = factorial*i;
+        }
+        printf("%d! = %llu\n", n, factorial);
+    }
+    
+    else if(pthread_equal(id,tid[1])==0)
+    {
+       n=arr[1];
+       for(i=1; i<=n; ++i)
+        {
+            factorial *= i;              // factorial = factorial*i;
+        }
+        printf("%d! = %llu\n", n, factorial);
+    }
+
+    else if(pthread_equal(id,tid[2])==0)
+    {
+	n=arr[2];
+       for(i=1; i<=n; ++i)
+        {
+            factorial *= i;              // factorial = factorial*i;
+        }
+        printf("%d! = %llu\n", n, factorial);
+	}
+    return NULL;
+}
+
+int main(int a, int b, int c)
+{
+    int i=0;
+    int err,n;
+	printf("./factorial ");
+    arr[0]=a;
+    arr[1]=b;
+    arr[2]=c;
+    while(i<3)
+    {
+        err=pthread_create(&(tid[i]),NULL,&factorial,NULL);//membuat thread
+        
+    }
+    pthread_join(tid[0],NULL);
+    pthread_join(tid[1],NULL);
+    pthread_join(tid[2],NULL);
+    return 0;
+}
