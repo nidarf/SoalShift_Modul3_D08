fb27f5bb7786bc7f03b252a68a517a2754fb838c
rizvisfbrina <32903576+rizvisfbrina@users.noreply.github.com>
2017-11-02 21:52:12 +0700

    revisi

--- prak5.c
+++ prak5.c
@@ -4,38 +4,39 @@
 #include<stdlib.h>
 #include<unistd.h>
 
-
-pthread_t tid[500];
-
-void *search(void *argv)
-{
+void *search(void *arg)
+{	
 	FILE *file;
 	int count=0;
-	char kata[1000];
-	char nama[100];
+	char baris[1024];
+	char *nama, *token;
+
+	nama=(char *) arg;
 
 	file = fopen("Novel.txt", "r");
-	strcpy(nama, argv);
-
-	while (!feof(file)){
-		fscanf (file, "%s", kata);
-		if (strstr(kata,argv) !=NULL) count++;
+	unsigned long i =0;
+	
+	while (fgets(baris,sizeof(baris),file)!=NULL){
+		token=strtok(baris," ,.");
+		while( token != NULL ) {
+	      	    if(strcmp(token,nama)==0) count++;
+		    token = strtok(NULL, " ,.");
+	   	}
 	}
-		
+	printf("%s=%d\n",nama,count);	
 	fclose(file);
-	printf("%s=%d\n",nama,count);
+return NULL;
 }
 
 int main(int argc, char **argv)
 {
-    int i=0;
+    int i=0;char *x;
+	pthread_t tid[argc];
 	for(i=1; i<argc; i++){
-		pthread_create(&tid[i], NULL, search, (void*)argv[i]);
-		}
-	for(i=0; i<argc; i++){
-		pthread_join(tid[i], NULL);
-		}	
-return 0; 
+		pthread_create(&tid[i], NULL, &search, (void*)argv[i]);
+		pthread_join(tid[i], NULL);		
+	}
+return 0;
 }
 
 
